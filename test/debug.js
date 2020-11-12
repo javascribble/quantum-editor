4bbe2098cdbc3cf4d73690b232ec54412a384494
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-11-12 12:45:42 -0500

    update input package

--- test/debug.js
+++ test/debug.js
@@ -3,7 +3,7 @@ import '/node_modules/@javascribble/quantum-layout/source/export.js';
 import '/node_modules/@javascribble/quantum-engine/source/export.js';
 import '/node_modules/@javascribble/quantum-canvas/source/export.js';
 import '/node_modules/@javascribble/quantum-loader/source/export.js';
-import '/node_modules/@javascribble/quantum-keyboard/source/export.js';
+import '/node_modules/@javascribble/quantum-input/source/export.js';
 import '/source/export.js';
 
 document.body.style.visibility = 'visible';
