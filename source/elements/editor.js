fee8c453ba5d010d686c4ff0fe1c3466081b0ad5
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-11-19 08:37:06 -0500

    start work on floating windows

--- source/elements/editor.js
+++ source/elements/editor.js
@@ -6,6 +6,24 @@ import html from '../templates/editor.js';
 export class Editor extends Component {
     static template = template(html);
 
+    constructor() {
+        super();
+
+        const windows = this.shadowRoot.querySelectorAll('.window');
+        for (const window of windows) {
+            window.addEventListener('dragstart', event => event.dataTransfer.setData('id', event.target));
+            window.addEventListener('drag', event => {
+                //Object.assign(window.style, { top: event.clientY, left: event.clientX });
+            });
+        }
+
+        this.addEventListener('dragover', event => event.preventDefault());
+        this.addEventListener('drop', event => {
+            event.preventDefault();
+            const id = event.dataTransfer.getData('id');
+        });
+    }
+
     slotChangedCallback(slot, addedElements, deletedElements) {
         //configureProject(this.shadowRoot,  );
         //configureLayout(this.shadowRoot, '');
--- source/templates/editor.js
+++ source/templates/editor.js
@@ -1,12 +1,19 @@
 export default `
 <style>
+    .window {
+        border: solid black 1px;
+    }
 </style>
-<quantum-layout stack>
-    <div id="header" class="bar">
+<quantum-layout>
+    <div id="window1" class="window">
+        <div draggable="true">header</div>
         <input id="load" type="file" />
         <button id="save">Save</button>
     </div>
-    <slot></slot>
+    <div id="window2" class="window">
+        <div draggable="true">header</div>
+        content
+    </div>
 </quantum-layout>
 <quantum-region id="selection"></quantum-region>
 `;
