18b767b07e5f0cbc3a134ad368179d29c15108a8
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-01-18 10:56:46 -0500

    update references

--- test/debug.js
+++ test/debug.js
@@ -5,6 +5,7 @@ import '/node_modules/@javascribble/quantum-region/source/main.js';
 import '/node_modules/@javascribble/quantum-loader/source/main.js';
 import '/node_modules/@javascribble/quantum-canvas/source/main.js';
 import '/node_modules/@javascribble/quantum-input/source/main.js';
+import '/node_modules/@javascribble/quantum-context/source/main.js';
 import '/node_modules/@javascribble/quantum-engine/source/main.js';
 import '/source/main.js';
 
