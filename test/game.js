6771f154da6ab8f95123bf94c8f912b1e616c8e2
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-02-16 06:43:08 -0500

    remove extension

--- test/game.js
+++ test/game.js
@@ -3,28 +3,10 @@ import { enablePlayerPlugin } from './plugins/player.js';
 import { enableScenePlugin } from './plugins/scene.js';
 import { enableTilePlugin } from './plugins/tile.js';
 
-const Engine = Quantum.types.find(type => type.name === 'Engine');
-Engine.plugins.add((api, engine) => {
+const engine = document.querySelector('quantum-engine');
+engine.plugins.add((api, engine) => {
     enableGamePlugin(api, engine);
     enablePlayerPlugin(api);
     enableScenePlugin(api);
     enableTilePlugin(api);
 });
-
-class UI extends Quantum { }
-
-UI.define('quantum-ui', ``);
-
-class Game extends Engine {
-    #ui = document.createElement('quantum-ui');
-
-    constructor() {
-        super();
-
-        this.appendChild(this.#ui);
-    }
-}
-
-Game.define('quantum-game');
-
-document.querySelector('quantum-canvas').addEventListener('resize', console.log);
