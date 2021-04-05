a8afa97e97f96ef50d8902331f352770d0ae8b66
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-04-05 09:52:26 -0400

    update sample

--- source/templates/editor.js
+++ source/templates/editor.js
@@ -23,27 +23,27 @@ export default `
 </style>
 <div id="header">
     <quantum-menu name="File">
-        <div invoke="testMethod"><span class="icon" title="save">&#128427;</span> Save</div>
-        <div invoke="testMethod"><span class="icon" title="export">&#128448;</span> Save As</div>
-        <div invoke="testMethod"><span class="icon" title="import">&#128449;</span> Open</div>
+        <div invoke="testMethod"><span title="save">&#128427;</span> Save</div>
+        <div invoke="testMethod"><span title="export">&#128448;</span> Save As</div>
+        <div invoke="testMethod"><span title="import">&#128449;</span> Open</div>
     </quantum-menu>
     <quantum-menu name="Plugins"></quantum-menu>
 </div>
 <slot></slot>
 <div id="footer">
-    <span class="icon" title="settings">&#128736;</span>
+    <span title="settings">&#128736;</span>
 
-    <span class="icon" title="play">&#9654;</span>
-    <span class="icon" title="pause">&#10074;&#10074;</span>
-    <span class="icon" title="stop">&#9209;</span>
+    <span title="play">&#9654;</span>
+    <span title="pause">&#10074;&#10074;</span>
+    <span title="stop">&#9209;</span>
 
-    <span class="icon" title="add">&#43;</span>
-    <span class="icon" title="delete">&#215;</span>
-    <span class="icon" title="edit">&#9998;</span>
+    <span title="add">&#43;</span>
+    <span title="delete">&#215;</span>
+    <span title="edit">&#9998;</span>
     
-    <span class="icon" title="transform">&#10021;</span>
-    <span class="icon" title="rotate">&#128472;</span>
-    <span class="icon" title="scale">&#9878;</span>
+    <span title="transform">&#10021;</span>
+    <span title="rotate">&#128472;</span>
+    <span title="scale">&#9878;</span>
 </div>
 
 <dialog>
--- test/game.js
+++ test/game.js
@@ -1,65 +1,57 @@
 document.querySelector('quantum-engine').plugins.push({
     load: async engine => {
-        const { canvas, input, entities, systems } = engine;
+        const { audio, video, input, entities, systems } = engine;
 
         systems.add({
-            validate: entity => 'player' in entity && 'world' in entity,
-            construct: entity => {
-                const { world, player } = entity;
-                const { tileset, divisor } = world;
-                const { sprite, spawn } = player;
-                const { sheet, size } = tileset;
-
-                const sprites = [];
-                for (let row = 0; row < sheet.height / size; row++) {
-                    for (let column = 0; column < sheet.width / size; column++) {
-                        sprites.push({ source: sheet, sx: column * size, sy: row * size, sw: size, sh: size });
-                    }
-                }
-
-                const indices = [];
-                for (let i = 0; i < divisor; i++) {
-                    for (let ii = 0; ii < divisor; ii++) {
-                        indices.push(Math.round(Math.random()));
-                    }
-                }
-
-                const children = [];
-                for (let index = 0; index < indices.length; index++) {
-                    const tile = { ...sprites[indices[index]] };
-                    tile.dx = tile.sw * (index % divisor);
-                    tile.dy = tile.sh * Math.floor(index / divisor);
-                    tile.dw = tile.sw;
-                    tile.dh = tile.sh;
-                    children.push(tile);
-                }
-
-                Object.assign(sprite, spawn);
-
-                children.push(sprite);
-                entity.children = children;
-            },
+            validate: entity => entity.player && entity.world,
             update: (entities, time) => {
                 for (const entity of entities) {
-                    const { player } = entity;
-                    const { sprite } = player;
+                    const { player, world } = entity;
+                    const { character } = player;
+                    const { position } = character;
 
                     if (input.getButton('ArrowUp')) {
-                        sprite.dy -= 5;
+                        position.y -= 5;
                     } else if (input.getButton('ArrowDown')) {
-                        sprite.dy += 5;
+                        position.y += 5;
                     } else if (input.getButton('ArrowLeft')) {
-                        sprite.dx -= 5;
+                        position.x -= 5;
                     } else if (input.getButton('ArrowRight')) {
-                        sprite.dx += 5;
+                        position.x += 5;
                     }
 
-                    canvas.drawImageTree(entity, 'children');
+                    video.drawImageTree(world);
                 }
             }
         });
 
-        entities.add(await engine.loadPrototype());
+        const root = await engine.loadPrototype();
+
+        const { world, player } = root;
+        const { tilemap, divisor } = world;
+        const { character, spawn } = player;
+
+        const sprites = engine.importUniformSprites(tilemap.image, tilemap.size);
+
+        world.children = [];
+        for (let i = 0; i < divisor; i++) {
+            for (let ii = 0; ii < divisor; ii++) {
+                const index = i * divisor + ii;
+                const sprite = sprites[Math.round(Math.random())];
+                world.children.push({
+                    ...sprite,
+                    position: {
+                        x: sprite.sw * (index % divisor),
+                        y: sprite.sh * Math.floor(index / divisor)
+                    }
+                });
+            }
+        }
+
+        Object.assign(character, spawn);
+        world.children.push(character);
+
+        entities.add(root);
     },
     unload: engine => { }
 });
