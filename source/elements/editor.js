0bc1891b0f1c0d2c9c1066731614fa736f2b625b
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-09-16 10:39:55 -0400

    update deps

--- source/elements/editor.js
+++ source/elements/editor.js
@@ -8,8 +8,6 @@ export class Editor extends Quantum {
 Editor.define('quantum-editor', editor);
 
 // import 'https://unpkg.com/@javascribble/quantum/bundles/main-window.js';
-// import 'https://unpkg.com/@javascribble/quantum-input/bundles/main.js';
-// import 'https://unpkg.com/@javascribble/quantum-canvas/bundles/main.js';
-// import 'https://unpkg.com/@javascribble/quantum-canvas/bundles/main-extensions.js';
-// import 'https://unpkg.com/@javascribble/quantum-engine/bundles/main.js';
-// import 'https://unpkg.com/@javascribble/quantum-engine/bundles/main-extensions.js';
+// import 'https://unpkg.com/@javascribble/quantum-input';
+// import 'https://unpkg.com/@javascribble/quantum-canvas';
+// import 'https://unpkg.com/@javascribble/quantum-engine';
--- test/debug.js
+++ test/debug.js
@@ -1,13 +1,9 @@
 import '/node_modules/@javascribble/quantum/bundles/main-window.js';
 import '/node_modules/@javascribble/quantum-engine/bundles/main.js';
-import '/node_modules/@javascribble/quantum-engine/bundles/main-extensions.js';
-import '/node_modules/@javascribble/quantum-engine/bundles/main-plugins.js';
 import '/node_modules/@javascribble/quantum-button/bundles/main.js';
 import '/node_modules/@javascribble/quantum-layout/bundles/main.js';
 import '/node_modules/@javascribble/quantum-tabs/bundles/main.js';
 import '/node_modules/@javascribble/quantum-canvas/bundles/main.js';
-import '/node_modules/@javascribble/quantum-canvas/bundles/main-extensions.js';
-import '/node_modules/@javascribble/quantum-canvas/bundles/main-plugins.js';
 import '/node_modules/@javascribble/quantum-input/bundles/main.js';
 import '/bundles/main.js';
 
