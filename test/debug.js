f2ca12bb1fb0b121425346d4c51cc113f66af1e8
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-09-27 13:23:45 -0400

    merge repositories

--- test/debug.js
+++ test/debug.js
@@ -3,8 +3,6 @@ import '/node_modules/@javascribble/quantum-engine/bundles/main.js';
 import '/node_modules/@javascribble/quantum-button/bundles/main.js';
 import '/node_modules/@javascribble/quantum-layout/bundles/main.js';
 import '/node_modules/@javascribble/quantum-tabs/bundles/main.js';
-import '/node_modules/@javascribble/quantum-canvas/bundles/main.js';
-import '/node_modules/@javascribble/quantum-input/bundles/main.js';
 import '/bundles/main.js';
 
 document.body.style.visibility = 'visible';
