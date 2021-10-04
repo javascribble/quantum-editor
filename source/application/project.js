214935a44fa160b4c3aeba3e1eece4151a3eaedb
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-10-04 09:54:31 -0400

    remove html export

--- source/application/project.js
+++ source/application/project.js
@@ -2,25 +2,9 @@ import { saveFile, loadFile } from '../utilities/file.js';
 
 const { getJson, setJson } = quantum;
 
-const htmlTemplate = html => `
-<html>
-    <head>
-        <style>
-            body {
-                visibility: hidden;
-                margin: 0;
-            }
-        </style>
-    </head>
-    <body>
-        <quantum-engine>${html}</quantum-engine>
-    </body>
-</html>
-`;
-
 export const configureProject = editor => {
     const { shadowRoot } = editor;
 
     shadowRoot.querySelector('#import').onclick = console.log;
-    shadowRoot.querySelector('#export').onclick = () => saveFile(htmlTemplate(''));
+    shadowRoot.querySelector('#export').onclick = console.log;
 };
